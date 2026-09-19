4b82e24c04142e4cb88b98c4540cc6dcc5182fb3
d0mkaaa <rutkauskasdomantas@gmail.com>
2026-09-20 00:09:50 +0300

    Simplify the build script and allow vendor lint attrs

--- src-tauri/vendor/glib/src/lib.rs
+++ src-tauri/vendor/glib/src/lib.rs
@@ -3,6 +3,8 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![allow(clippy::missing_safety_doc)]
 #![allow(renamed_and_removed_lints)]
+#![allow(mismatched_lifetime_syntaxes)]
+#![allow(unused_parens)]
 #![doc = include_str!("../README.md")]
 
 pub use bitflags;
